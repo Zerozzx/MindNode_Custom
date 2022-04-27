eef37b615f9ff1ea3af01c419ed28fc7490ce994
Zerozzx <654245529@qq.com>
2022-04-27 23:28:54 +0800

    添加注释，尝试功能

--- main.ts
+++ main.ts
@@ -2,19 +2,22 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
+
 interface MyPluginSettings {
-	mySetting: string;
+	WelcomeStr: string;// 插件的设置变量，把需要保存的一些变量放在里面
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	WelcomeStr: '欢迎来到 Obsidian'// 对上面设置的变量给一个默认值
 }
 
+// 插件的主要功能设置
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
+	//当插件开始加载时调用
 	async onload() {
-		await this.loadSettings();
+		await this.loadSettings();// 插件在被初始化的时候会加载设置内容
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
@@ -27,6 +30,15 @@ export default class MyPlugin extends Plugin {
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
+
+		this.addCommand({
+			id: 'simple-plugin-demo-command1',
+			name: '尝试添加命令',
+			callback: () => { 
+				new Notice(this.settings.WelcomeStr);//创建一个新通知
+				new SampleModal(this.app).open();//打开属于自己的面板
+			}
+		});
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -66,7 +78,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SampleSettingTab(this.app, this));//这一步就在插件设置列表中添加了我们的 “插件设置面板”
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -78,35 +90,46 @@ export default class MyPlugin extends Plugin {
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
+	//当插件开始卸载时调用
 	onunload() {
 
 	}
 
+	//加载 插件设置
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
+	//保存 插件设置
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 }
 
+//Modal：实现接口 CloseableComponent (可关闭组件) 的类，相当于 “面板”，程序构建实例的时候，会弹出一个面板，就是实例化的这个类对象
 class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
+	//当插件面板被打开
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		
+		const title = this.titleEl;
+		title.setText("当前打开的文档名称");
+		
+		const { contentEl } = this;
+		contentEl.setText(this.app.workspace.getActiveFile().name);// 获取当前激活的文件名称
 	}
 
+	//当插件面板被关闭
 	onClose() {
 		const {contentEl} = this;
 		contentEl.empty();
 	}
 }
 
+// 插件设置页面，这个类负责构造插件 “设置页面” 的面板内容，可以在这个类中 对 “MyPluginSettings” 中设置的变量赋值
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
@@ -115,23 +138,24 @@ class SampleSettingTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
+	//动态生成一个 html 页面
 	display(): void {
 		const {containerEl} = this;
 
-		containerEl.empty();
+		containerEl.empty();//初始化容器
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});//构造一个 h2 标题
 
-		new Setting(containerEl)
+		new Setting(containerEl) //构造一个设置项
 			.setName('Setting #1')
 			.setDesc('It\'s a secret')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.WelcomeStr)
 				.onChange(async (value) => {
 					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+					this.plugin.settings.WelcomeStr = value;
+					await this.plugin.saveSettings();// 构造了一个 Setting 的输出框，然后传入了一个 lambda 函数到 OnChange 函数内，用来响应数值改变后要做什么操作
 				}));
 	}
 }
